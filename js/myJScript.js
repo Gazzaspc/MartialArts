fe9caf4bdd041bfd3e47cba00d5a96c8d0ebed0d
Gary Kirby <gazzaspc@gmail.com>
2021-05-31 18:24:53 +0100

    First commit

---
+++ js/myJScript.js
@@ -0,0 +1,16 @@
+﻿////Javascript in here
+
+function resetBlocker() {
+    var myBlocker = document.getElementsByClassName("blocker");
+    var myHeader = document.getElementById("header");
+
+    for (x of myBlocker) {
+        x.style.height = myHeader.offsetHeight + "px";
+        //alert("1 " + x.style.height);
+        //alert("2 " + myHeader.offsetHeight);
+        /*document.getElementById("pSize").innerHTML = myHeader.offsetHeight + " x = " + x.style.height;*/
+    }
+}
+
+resetBlocker();
+window.onresize = resetBlocker;
